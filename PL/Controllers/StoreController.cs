e14c4399d54fb983334d8a632fb09c61d970bc1f
HugoBznDIGIS <hbzn14@gmail.com>
2024-01-17 18:32:23 -0600

    Consumo de API y mejora de codigo

--- PL/Controllers/StoreController.cs
+++ PL/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using PL.Models;
 
 namespace PL.Controllers
@@ -13,32 +14,30 @@ namespace PL.Controllers
             return View(producto);
         }
 
-        public IActionResult AddCart(int idProducto) 
+        public IActionResult AddCart(int idProducto)
         {
             bool existe = false;
             VentaModel ventaModel = new VentaModel();
             ventaModel.Carrito = new List<object>();
-            BL.Producto producto = BL.Producto.GetById(idProducto);
+            BL.Producto producto = GetItem(idProducto);
 
-            if (HttpContext.Session.GetString("Carrito") == null) 
+            if (HttpContext.Session.GetString("Carrito") == null)
             {
                 if (producto != null)
                 {
-                    BL.Producto productoItem = producto;
-                    productoItem.Cantidad = 1;
-                    ventaModel.Carrito.Add(productoItem);
+                    producto.Cantidad = 1;
+                    ventaModel.Carrito.Add(producto);
                     HttpContext.Session.SetString("Carrito", Newtonsoft.Json.JsonConvert.SerializeObject(ventaModel.Carrito));
                     ViewBag.Mensaje = "Se ha agregado correctamente";
                 }
             }
             else
             {
-                BL.Producto productoItem = producto;
                 GetCart(ventaModel);
 
                 foreach (BL.Producto itemProducto in ventaModel.Carrito)
                 {
-                    if (productoItem.IdProducto == itemProducto.IdProducto)
+                    if (producto.IdProducto == itemProducto.IdProducto)
                     {
                         itemProducto.Cantidad += 1;
                         existe = true;
@@ -56,8 +55,8 @@ namespace PL.Controllers
                 }
                 else
                 {
-                    productoItem.Cantidad = 1;
-                    ventaModel.Carrito.Add(productoItem);
+                    producto.Cantidad = 1;
+                    ventaModel.Carrito.Add(producto);
                     HttpContext.Session.SetString("Carrito", Newtonsoft.Json.JsonConvert.SerializeObject(ventaModel.Carrito));
                     ViewBag.Mensaje = "Se ha agregado correctamente un nuevo elemento";
                 }
@@ -99,6 +98,40 @@ namespace PL.Controllers
         {
             HttpContext.Session.Remove("Carrito");
             return RedirectToAction("Cart");
+        }
+
+        private static readonly string baseURL = "http://localhost:5295/api/Producto";
+        public static BL.Producto GetItem(int idProducto)
+        {
+            BL.Producto producto = new BL.Producto();
+
+            using (var client = new HttpClient())
+            {
+                var responseTask = client.GetAsync($"{baseURL}/{idProducto}");
+                responseTask.Wait();
+
+                var resultService = responseTask.Result;
+
+                if (resultService.IsSuccessStatusCode)
+                {
+                    var readTask = resultService.Content.ReadAsStringAsync();
+                    dynamic resultJSON = JObject.Parse(readTask.Result);
+                    readTask.Wait();
+
+                    producto.IdProducto = resultJSON["idProducto"];
+                    producto.Nombre = resultJSON["nombre"];
+                    producto.CodigoBarras = resultJSON["codigoBarras"];
+                    producto.Descripcion = resultJSON["descripcion"];
+                    producto.Precio = resultJSON["precio"];
+                    producto.Categoria = new BL.Categoria();
+                    producto.Categoria.IdCategoria = resultJSON["categoria"]["idCategoria"];
+                    producto.Categoria.Nombre = resultJSON["categoria"]["nombre"];
+                    producto.PrecioVenta = resultJSON["precioVenta"];
+                    producto.Descuento = resultJSON["descuento"];
+                }
+            }
+
+            return producto;
         }
     }
 }
